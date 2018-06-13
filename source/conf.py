3d5545afa669d3d3598aa8a68e06a92d4c7053b9
hansp <hans.vanpiggelen@surfsara.nl>
2018-06-13 12:34:48 +0200

    Enable CSS theming

--- source/conf.py
+++ source/conf.py
@@ -193,7 +193,7 @@ html_title = "{} v{}".format(project, release)
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'TDRDocumentationdoc'
+htmlhelp_basename = 'SURFDataRepositoryDocumentationDoc'
 
 
 # -- Options for LaTeX output ---------------------------------------------
